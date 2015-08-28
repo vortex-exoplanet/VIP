e6c26b615eab7d8a06393b42e11c7a46e01cf775
Valentin Christiaens <valentinchrist@hotmail.com>
2015-08-28 12:50:12 -0400

    Modifications after 2nd skype meeting (mainly derotation and derot_angle related)

--- vip/calib/__init__.py
+++ vip/calib/__init__.py
@@ -6,6 +6,7 @@ from badframes import *
 from cosmetics import *
 from cosmetics_ifs import *
 from derotation import *
+from parangles import *
 from recentering import *
 from rescaling import *
 from subsampling import *

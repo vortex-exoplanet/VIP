0d176d1da8712899d64c58b00bc53228a795ef27
Carlos Gomez <carlosgg33@gmail.com>
2018-04-10 12:45:00 +0200

    Improved HCICube class

--- vip_hci/hci_cube.py
+++ vip_hci/hci_cube.py
@@ -9,19 +9,42 @@ from __future__ import division, print_function
 __author__ = 'Carlos Alberto Gomez Gonzalez'
 __all__ = ['HCICube']
 
+import numpy as np
 from .fits import open_fits, write_fits, append_extension
 from .preproc import (cube_collapse, cube_crop_frames, cube_derotate,
                       cube_drop_frames, cube_detect_badfr_correlation,
                       cube_detect_badfr_pxstats, cube_px_resampling,
                       cube_subsample, cube_recenter_2dfit,
                       cube_recenter_dft_upsampling)
-from .var import cube_filter_highpass, cube_filter_lowpass, mask_circle
+from .var import (cube_filter_highpass, cube_filter_lowpass, mask_circle,
+                  pp_subplots)
 from .stats import cube_basic_stats, cube_distance
 from .phot import cube_inject_companions
 
 
+# TODO: add a HCIFrame class
+
+
 class HCICube:
     """ High-contrast imaging sequence/cube class.
+
+    Parameters
+    ----------
+    cube : str or numpy array
+        3d or 4d high-contrast image sequence. If a string is provided, cube is
+        interpreted as the path of the FITS file containing the sequence.
+    hdu : int
+        If ``cube`` is a String, ``hdu`` indicates the HDU from the FITS file.
+    angles : list or numpy array
+        The vector of parallactic angles.
+    scaling : list or numpy array
+        The vector of scaling factors.
+    fwhm : float
+        The FWHM associated with this dataset (instrument dependent).
+    px_scale : float
+        The pixel scale associated with this dataset (instrument dependent).
+    psf : numpy array
+        The PSF template associated with this datset.
     """
     def __init__(self, cube, hdu=0, angles=None, scaling=None, fwhm=None,
                  px_scale=None, psf=None):
@@ -82,6 +105,8 @@ class HCICube:
 
     def collapse(self, mode='median', n=50):
         """ Collapsing the sequence into a 2d array.
+
+        # TODO: support 4d case.
         """
         frame = cube_collapse(self.array, mode, n)
         print('Cube successfully collapsed')
@@ -89,6 +114,8 @@ class HCICube:
 
     def crop_frames(self, size, xy=None, force=False):
         """ Cropping the frames of the sequence.
+
+        # TODO: support 4d case.
         """
         self.array = cube_crop_frames(self.array, size, xy, force)
 
@@ -107,12 +134,17 @@ class HCICube:
     def drop_frames(self, n, m):
         """ Slicing the cube using the `n` (initial) and `m` (final) indices in
         a 1-indexed fashion.
+
+        # TODO: support 4d case.
         """
         self.array = cube_drop_frames(self.array, n, m, self.angles)
 
     def filter(self, method, mode, median_size=5, kernel_size=5, fwhm_size=5,
                btw_cutoff=0.2, btw_order=2, gauss_mode='conv', verbose=True):
-        """
+        """ High/low pass filtering the frames of the cube.
+
+        # TODO: support 4d case.
+
         Parameters
         ----------
         method : {'lp', 'hp'}
@@ -132,7 +164,10 @@ class HCICube:
 
     def frame_distances(self, frame, region='full', dist='sad',
                         inner_radius=None, width=None, plot=True):
-        """
+        """ Calculating the frame distance/correlation with respect to a
+        reference image.
+
+        # TODO: support 4d case.
         """
         _ = cube_distance(self.array, frame, region, dist, inner_radius, width,
                           plot)
@@ -140,6 +175,8 @@ class HCICube:
     def frame_stats(self, region='circle', radius=5, xy=None, inner_radius=0,
                     size=5, plot=False):
         """ Getting statistics in ``region`` of the cube.
+
+        # TODO: support 4d case.
         """
         _ = cube_basic_stats(self.array, region, radius, xy, inner_radius, size,
                              plot, False)
@@ -147,7 +184,7 @@ class HCICube:
     def inject_companions(self, flux, rad_dists, n_branches=1,
                           theta=0, imlib='opencv', interpolation='lanczos4',
                           verbose=True):
-        """ Injection of fake companions.
+        """ Injection of fake companions in 3d or 4d cubes.
 
         # TODO: support the injection of a Gaussian/Moffat kernel.
         # TODO: return array/HCICube object instead?
@@ -168,13 +205,108 @@ class HCICube:
         """
         if isinstance(angles, str):
             self.angles = open_fits(angles, hdu)
-        else:
+        elif isinstance(angles, (list, np.ndarray)):
             self.angles = angles
+        else:
+            msg = '`Angles` has a wrong type. Must be a list or 2d np.ndarray'
+            raise ValueError(msg)
+
+    def load_scaling_factors(self, scaling, hdu):
+        """ Loads the scaling factors vector from a FITS file. It is possible to
+        specify the HDU.
+        """
+        if isinstance(scaling, str):
+            self.scaling = open_fits(scaling, hdu)
+        elif isinstance(scaling, (list, np.ndarray)):
+            self.scaling = scaling
+        else:
+            msg = '`Scaling` has a wrong type. Must be a list or 2d np.ndarray'
+            raise ValueError(msg)
 
     def mask_center(self, radius, fillwith=0, mode='in'):
         """ Masking the values inside/outside a centered circular aperture.
+
+        # TODO: support 4d case.
         """
         self.array = mask_circle(self.array, radius, fillwith, mode)
+
+    def plot(self, **kwargs):
+        """ Plotting ``maxplots`` frames of the cube (3D case).
+
+        # TODO: add arguments to slice the cube?
+        # TODO: support 4d case.
+
+        Parameters in **kwargs
+        ----------------------
+        angscale : bool
+            If True, the axes are displayed in angular scale (arcsecs).
+        angticksep : int
+            Separation for the ticks when using axis in angular scale.
+        arrow : bool
+            To show an arrow pointing to input px coordinates.
+        arrowalpha : float
+            Alpha transparency for the arrow.
+        arrowlength : int
+            Length of the arrow, 20 px by default.
+        arrowshiftx : int
+            Shift in x of the arrow pointing position, 5 px by default.
+        axis : bool
+            Show the axis, on by default.
+        circle : list of tuples
+            To show a circle at given px coordinates, list of tuples.
+        circlerad : int
+            Radius of the circle, 6 px by default.
+        cmap : str
+            Colormap to be used, 'viridis' by default.
+        colorb : bool
+            To attach a colorbar, on by default.
+        cross : tuple of float
+            If provided, a crosshair is displayed at given px coordinates.
+        crossalpha : float
+            Alpha transparency of thr crosshair.
+        dpi : int
+            Dots per inch, for plot quality.
+        getfig : bool
+            Returns the matplotlib figure.
+        grid : bool
+            If True, a grid is displayed over the image, off by default.
+        gridalpha : float
+            Alpha transparency of the grid.
+        gridcolor : str
+            Color of the grid lines.
+        gridspacing : int
+            Separation of the grid lines in pixels.
+        horsp : float
+            Horizontal gap between subplots.
+        label : str or list of str
+            Text for annotating on subplots.
+        labelpad : int
+            Padding of the label from the left bottom corner.
+        labelsize : int
+            Size of the labels.
+        log : bool
+            Log colorscale.
+        maxplots : int
+            When the input (*args) is a 3d array, maxplots sets the number of
+            cube slices to be displayed.
+        pxscale : float
+            Pixel scale in arcseconds/px. Default 0.01 for Keck/NIRC2.
+        rows : int
+            How many rows (subplots in a grid).
+        save : str
+            If a string is provided the plot is saved using this as the path.
+        showcent : bool
+            To show a big crosshair at the center of the frame.
+        title : str
+            Title of the plot(s), None by default.
+        vmax : int
+            For stretching the displayed pixels values.
+        vmin : int
+            For stretching the displayed pixels values.
+        versp : float
+            Vertical gap between subplots.
+        """
+        pp_subplots(self.array, **kwargs)
 
     def recenter(self, method='2dfit', xy=None, subi_size=5, model='gauss',
                  nproc=1, imlib='opencv', interpolation='lanczos4',
@@ -210,8 +342,8 @@ class HCICube:
 
         Parameters
         ----------
-        method : str, optional
-            corr, pxstats
+        method : {'corr', 'pxstats'}, str optional
+
         """
         if method == 'corr':
             if frame_ref is None:

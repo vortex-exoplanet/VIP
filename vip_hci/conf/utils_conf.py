06c4d4d21785a71289018b410d8d54ba9eb95812
RFarkas <r4lv@peaxels.com>
2018-05-18 11:14:09 +0200

    FEAT: helper functions for parallel computing

--- vip_hci/conf/utils_conf.py
+++ vip_hci/conf/utils_conf.py
@@ -6,12 +6,17 @@ Module with utilities.
 
 from __future__ import division, print_function
 
-__author__ = 'Carlos Alberto Gomez Gonzalez'
-__all__ = ['Progressbar']
+__author__ = 'Carlos Alberto Gomez Gonzalez, Ralf Farkas'
+__all__ = ['Progressbar',
+           'pool_map', 'pool_imap', 'fixed',
+           'make_chunks']
 
 import os
 import sys
 import numpy as np
+
+import itertools as itt
+from multiprocessing import Pool
 
 sep = '-' * 80
 vip_figsize = (10, 5)
@@ -150,6 +155,84 @@ def check_array(input, dim=1, name=None):
 def eval_func_tuple(f_args):
     """ Takes a tuple of a function and args, evaluates and returns result"""
     return f_args[0](*f_args[1:])                       
+
+
+class FixedObj():
+    def __init__(self, v):
+        self.v = v
+
+def fixed(v):
+    return FixedObj(v)
+
+
+def pool_map(nproc, fkt, *args, msg=None, verbose=True,
+                   progressbar_single=True, generator=False):
+    """
+    pool.map which uses `fixed()` for ...
+
+    """
+    
+    args_r = [a.v if isinstance(a, FixedObj) else itt.repeat(a) for a in args]
+    z = zip(itt.repeat(fkt), *args_r)
+
+    if nproc == 1:
+        if progressbar_single:
+            total = len([a.v for a in args if isinstance(a, FixedObj)][0])
+            z = Progressbar(z, desc=msg, verbose=verbose, total=total)
+
+        res = map(eval_func_tuple, z)
+        if not generator:
+            res = list(res)
+
+
+    else:
+        if verbose and msg is not None:
+            print(msg+" with {} processes".format(nproc))
+        pool = Pool(processes=nproc)
+        if generator:
+            res = pool.imap(eval_func_tuple, z)
+        else:
+            res = pool.map(eval_func_tuple, z)
+        pool.close()
+        pool.join()
+
+    return res
+
+
+def pool_imap(nproc, fkt, *args, msg=None, verbose=True,
+                   progressbar_single=True):
+    return pool_map(nproc, fkt, *args, msg=msg, verbose=verbose,
+                    progressbar_single=progressbar_single, generator=True)
+
+
+
+
+def repeat(*args):
+    """
+
+    Examples
+    --------
+
+    # instead of using
+
+    import itertools as itt
+    
+    my_fkt(itt.repeat(a), itt.repeat(b), itt.repeat(c), d, itt.repeat(e))
+
+    # you could use `repeat`:
+
+    my_fkt(*repeat(a, b, c), d, *repeat(e))
+
+    """
+    return [itt.repeat(a) for a in args]
+
+
+def make_chunks(l, n):
+    """
+    make_chunks(range(13), 3)
+        -> [[0, 3, 6, 9, 12], [1, 4, 7, 10], [2, 5, 8, 11]]
+    """
+    return [l[i::n] for i in range(n)]
 
 
 class redirect_output(object):

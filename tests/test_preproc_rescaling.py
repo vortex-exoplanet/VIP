5f8e056d1e56bec5e940a7edaf473f08f8f266fa
RFarkas <r4lv@peaxels.com>
2018-09-19 10:31:38 +0200

    TST: tests for other preproc.rescaling functions
    - cube_px_resampling
    - frame_px_resampling
    - cube_rescaling_wavelengths
    - check_scal_vector

--- tests/test_preproc_rescaling.py
+++ tests/test_preproc_rescaling.py
@@ -8,11 +8,102 @@ from __future__ import division, print_function
 __author__ = "Ralf Farkas"
 
 import numpy as np
-from vip_hci.preproc.rescaling import _find_indices_sdi
+from vip_hci.preproc.rescaling import (cube_px_resampling,
+                                       frame_px_resampling,
+                                       cube_rescaling_wavelengths,
+                                       check_scal_vector,
+                                       _find_indices_sdi)
 
 from helpers import aarc
-from pytest import mark
+from pytest import mark, raises
 parametrize = mark.parametrize
+
+
+CUBE = np.ones((10, 100, 100))
+FRAME = np.zeros((100, 100))
+
+
+@parametrize("imlib", ["ndimage", "opencv"])
+def test_cube_px_resampling(imlib):
+
+    # === enlargen ===
+
+    res = cube_px_resampling(CUBE, scale=2, imlib=imlib)
+    assert res.shape == (10, 200, 200)
+
+    # === shrink ===
+
+    res = cube_px_resampling(CUBE, scale=0.5, imlib=imlib)
+    assert res.shape == (10, 50, 50)
+
+
+@parametrize("imlib", ["ndimage", "opencv"])
+def test_frame_px_resampling(imlib):
+    """
+
+    Notes
+    -----
+    Testing of the different ``interpolation`` is not needed, we rely on the
+    external modules here. The calling signature is the same, so there is not
+    much VIP can do.
+    """
+
+    # === enlargen ===
+    res = frame_px_resampling(FRAME, scale=2, imlib=imlib, verbose=True)
+    assert res.shape == (200, 200)
+
+    # === shrink ===
+    res = frame_px_resampling(FRAME, scale=0.5, imlib=imlib, verbose=True)
+    assert res.shape == (50, 50)
+
+
+@parametrize("imlib,interpolation",
+             [
+                 ("opencv", "lanczos4"),
+                 ("ndimage", "bicubic"),
+             ])
+def test_cube_rescaling_wavelengths(imlib, interpolation):
+    scal_list = np.arange(10) + 1  # no zero
+
+    # === basic function ===
+
+    res1 = cube_rescaling_wavelengths(CUBE, scal_list, imlib=imlib,
+                                      interpolation=interpolation)
+    cube1, med1, y1, x1, cy1, cx1 = res1
+
+    assert cube1.shape == (10, 1000, 1000)  # frame size x10 x10
+
+    for i in range(cube1.shape[0]):
+        aarc(cube1[i].mean() * scal_list[i]**2, 1)
+
+    # === undo ===
+
+    res2 = cube_rescaling_wavelengths(cube1, scal_list, imlib=imlib,
+                                      interpolation=interpolation,
+                                      inverse=True, x_in=100, y_in=100)
+    cube2, med2, y2, x2, cy2, cx2 = res2
+
+    aarc(cube2, CUBE)
+
+
+def test_check_scal_vector():
+    scal_vec = np.array([2, 8, 4])
+
+    # === basic function ===
+
+    res = check_scal_vector(scal_vec)
+    truth = np.array([4, 1, 2])
+
+    aarc(res, truth)
+
+    # === do nothing if min factor is already 1 ===
+
+    res2 = check_scal_vector(res)
+    aarc(res2, res)
+
+    # === wrong input value ===
+    with raises(TypeError):
+        check_scal_vector(42)
 
 
 @parametrize("dist, index_ref, truth",

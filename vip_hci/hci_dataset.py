a13fc292e9691ba370121d45667aee9c5040751b
RFarkas <r4lv@peaxels.com>
2018-10-15 17:10:30 +0200

    FIX: cube_drop_frames returns tuple

--- vip_hci/hci_dataset.py
+++ vip_hci/hci_dataset.py
@@ -651,7 +651,7 @@ class HCIDataset(Saveable):
 
         # TODO: support 4d case, documentation
         """
-        self.cube = cube_drop_frames(self.cube, n, m, self.angles)
+        self.cube, self.angles = cube_drop_frames(self.cube, n, m, self.angles)
 
     def filter(self, method, mode, median_size=5, kernel_size=5, fwhm_size=5,
                btw_cutoff=0.2, btw_order=2, gauss_mode='conv', verbose=True):

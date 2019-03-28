e17f685c58024ec4bdeca6c8598c1f46672585a9
Carlos Gomez <carlosgg33@gmail.com>
2019-03-28 15:50:12 +0100

    removing  from __future__ imports in exlib module

--- vip_hci/exlib/__init__.py
+++ vip_hci/exlib/__init__.py
@@ -2,7 +2,7 @@
 Subpackage ``exlib`` contains external libraries self contained in VIP:
 IUWT from https://github.com/ratt-ru/PyMORESANE/
 """
-from __future__ import absolute_import
+
 
 #from .ds9 import *
 from .iuwt import *

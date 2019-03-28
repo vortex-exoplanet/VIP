39d008afd747188c1ee65a8c840ec1014fa67289
Carlos Gomez <carlosgg33@gmail.com>
2019-03-28 15:52:53 +0100

    removing  from __future__ imports in var subpackage

--- vip_hci/var/fit_2d.py
+++ vip_hci/var/fit_2d.py
@@ -3,7 +3,7 @@
 """
 2d fitting and creation of synthetic PSFs.
 """
-from __future__ import division, print_function
+
 
 __author__ = 'Carlos Alberto Gomez Gonzalez'
 __all__ = ['create_synth_psf',

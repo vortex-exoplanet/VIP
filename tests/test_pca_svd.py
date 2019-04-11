02f4fa771afb50e032cba03b95550e0c9cdf5b3a
Carlos Gomez <carlosgg33@gmail.com>
2019-04-11 08:49:55 +0200

    fixing tests

--- tests/test_pca_svd.py
+++ tests/test_pca_svd.py
@@ -14,7 +14,8 @@ def test_svd_recons():
     random_state = np.random.RandomState(42)
     mat = random_state.randn(20, 100)
     ncomp = 20
-    U, S, V = svd_wrapper(mat, 'lapack', ncomp, False, False, usv=True)
+    U, S, V = svd_wrapper(mat, mode='lapack', ncomp=ncomp, verbose=False,
+                          full_output=True)
     print(U.shape)
     print(S.shape)
     print(V.shape)

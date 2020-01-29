6dad02bcf977ed6e0065e95c32c682cfc2b56fb1
VaChristiaens <valentinchristiaens@gmail.com>
2020-01-30 01:50:27 +1100

    Adapted pca_local function to be compatible with contrast curve function

--- vip_hci/pca/pca_local.py
+++ vip_hci/pca/pca_local.py
@@ -150,7 +150,7 @@ def pca_annular(cube, angle_list, cube_ref=None, scale_list=None, radius_int=0,
         ``spat-mean``: spatial mean is subtracted.
 
         ``temp-standard``: temporal mean centering plus scaling pixel values
-        to unit variance.
+        to unit variance. HIGHLY RECOMMENDED FOR ASDI AND RDI CASES.
 
         ``spat-standard``: spatial mean centering plus scaling pixel values
         to unit variance.
@@ -376,11 +376,11 @@ def _pca_adi_rdi(cube, angle_list, radius_int=0, fwhm=4, asize=2, n_segments=1,
         delta_rot = [delta_rot] * n_annuli
 
     # forcing the 'temp-standard' scaling
-    if cube_ref is not None:
-        if not scaling == 'temp-standard':
-            scaling = 'temp-standard'
-            if verbose:
-                print("Pixel-wise scaling set to `temp-standard`")
+#    if cube_ref is not None:
+#        if not scaling == 'temp-standard':
+#            scaling = 'temp-standard'
+#            if verbose:
+#                print("Pixel-wise scaling set to `temp-standard`")
 
     if isinstance(n_segments, int):
         n_segments = [n_segments for _ in range(n_annuli)]

0cfd51cf21469449e46326a6d8340b519866336b
VChristiaens <valentinchrist@hotmail.com>
2021-07-22 16:19:43 +1000

    added annular version of NMF

--- vip_hci/nmf/nmf_fullfr.py
+++ vip_hci/nmf/nmf_fullfr.py
@@ -11,18 +11,21 @@ import numpy as np
 from sklearn.decomposition import NMF
 from ..preproc import cube_derotate, cube_collapse
 from ..preproc.derotation import _compute_pa_thresh, _find_indices_adi
-from ..var import prepare_matrix, reshape_matrix, frame_center, dist
+from ..var import (prepare_matrix, reshape_matrix, frame_center, dist, 
+                   matrix_scaling)
 from ..conf import timing, time_ini
 
 
 def nmf(cube, angle_list, cube_ref=None, ncomp=1, scaling=None, max_iter=100,
         random_state=None, mask_center_px=None, source_xy=None, delta_rot=1, 
-        fwhm=4, imlib='opencv', interpolation='lanczos4', collapse='median', 
-        full_output=False, verbose=True, cube_sig=None, **kwargs):
+        fwhm=4, init_svd='nndsvd', imlib='opencv', interpolation='lanczos4', 
+        collapse='median', full_output=False, verbose=True, cube_sig=None, 
+        handle_neg='mask', **kwargs):
     """ Non Negative Matrix Factorization for ADI sequences. Alternative to the
     full-frame ADI-PCA processing that does not rely on SVD or ED for obtaining
     a low-rank approximation of the datacube. This function embeds the 
-    scikit-learn NMF algorithm solved through coordinate descent method. 
+    scikit-learn NMF algorithm solved through either the coordinate descent or 
+    the multiplicative update method.
     
     Parameters
     ----------
@@ -49,6 +52,23 @@ def nmf(cube, angle_list, cube_ref=None, ncomp=1, scaling=None, max_iter=100,
     mask_center_px : None or int
         If None, no masking is done. If an integer > 1 then this value is the
         radius of the circular mask.
+    source_xy : tuple of int, optional
+        For ADI-PCA, this triggers a frame rejection in the PCA library, with
+        ``source_xy`` as the coordinates X,Y of the center of the annulus where
+        the PA criterion is estimated. When ``ncomp`` is a tuple, a PCA grid is
+        computed and the S/Ns (mean value in a 1xFWHM circular aperture) of the
+        given (X,Y) coordinates are computed.
+    delta_rot : int, optional
+        Factor for tunning the parallactic angle threshold, expressed in FWHM.
+        Default is 1 (excludes 1xFHWM on each side of the considered frame).
+    fwhm : float, optional
+        Known size of the FHWM in pixels to be used. Default value is 4. 
+    init_svd: str, optional {'nnsvd','nnsvda','random'}
+        Method used to initialize the iterative procedure to find H and W.
+        'nndsvd': non-negative double SVD recommended for sparseness
+        'nndsvda': NNDSVD where zeros are filled with the average of cube; 
+        recommended when sparsity is not desired
+        'random': random initial non-negative matrix
     imlib : str, optional
         See the documentation of the ``vip_hci.preproc.frame_rotate`` function.
     interpolation : str, optional
@@ -61,7 +81,8 @@ def nmf(cube, angle_list, cube_ref=None, ncomp=1, scaling=None, max_iter=100,
     verbose : {True, False}, bool optional
         If True prints intermediate info and timing. 
     kwargs 
-        Additional arguments for scikit-learn NMF algorithm.             
+        Additional arguments for scikit-learn NMF algorithm. See:
+        https://scikit-learn.org/stable/modules/generated/sklearn.decomposition.NMF.html             
              
     Returns
     -------
@@ -70,71 +91,99 @@ def nmf(cube, angle_list, cube_ref=None, ncomp=1, scaling=None, max_iter=100,
     the residuals derotated and the final frame.     
     
     """
-    array = cube
+    array = cube.copy()
     if verbose:  
         start_time = time_ini()
     n, y, x = array.shape
     
-    matrix = prepare_matrix(cube, scaling, mask_center_px, mode='fullfr',
-                            verbose=verbose)
-    matrix += np.abs(matrix.min())
-    if cube_ref is not None:
-        matrix_ref = prepare_matrix(cube_ref, scaling, mask_center_px,
-                                    mode='fullfr', verbose=verbose)
-        matrix_ref += np.abs(matrix_ref.min())         
-    elif source_xy is None or delta_rot is None:
-        matrix_ref = matrix.copy()
+    matrix_ref=None
+    matrix_sig=None
+    
+    # how to handle negative values
+    if handle_neg == 'mask':
+        yy, xx = np.where(np.amin(array,axis=0)>0)
+        if len(yy)>0:
+            matrix = array[:,yy,xx]
+            matrix = matrix_scaling(matrix, scaling)
+            if cube_ref is not None:
+                matrix_ref = cube_ref[:,yy,xx]
+                matrix_ref = matrix_scaling(matrix_ref, scaling)
+            if cube_sig is not None:
+                matrix_sig = cube_sig[:,yy,xx]
+        else:
+            raise ValueError("Remove frame(s) with negative values")
     else:
-        # rotation threshold applied and NMF repeated for each frame on different matrices
-        if delta_rot is None or fwhm is None:
-            msg = 'Delta_rot or fwhm parameters missing. Needed for the'
-            msg += 'PA-based rejection of frames from the library'
-            raise TypeError(msg)
-        residuals_cube = np.zeros_like(cube)
-        recon_cube = np.zeros_like(cube)
-        yc, xc = frame_center(cube[0], False)
-        x1, y1 = source_xy
-        ann_center = dist(yc, xc, y1, x1)
-        pa_thr = _compute_pa_thresh(ann_center, fwhm, delta_rot)
-        mid_range = np.abs(np.amax(angle_list) - np.amin(angle_list))/2
-        if pa_thr >= mid_range - mid_range * 0.1:
-            new_pa_th = float(mid_range - mid_range * 0.1)
-            if verbose:
-                msg = 'PA threshold {:.2f} is too big, will be set to '
-                msg += '{:.2f}'
-                print(msg.format(pa_thr, new_pa_th))
-            pa_thr = new_pa_th
+        if handle_neg=='null':
+            array[np.where(array<0)]=0
+        elif handle_neg=='subtr_min':
+            array -= np.amin(array)
+        elif not handle_neg=='mask':
+            raise ValueError("Mode to handle neg. pixels not recognized")
+    
+        matrix = prepare_matrix(array, scaling, mask_center_px, mode='fullfr',
+                                verbose=verbose)
+        if cube_ref is not None:
+            matrix_ref = prepare_matrix(cube_ref, scaling, mask_center_px,
+                                        mode='fullfr', verbose=verbose)
+            matrix_ref = matrix_ref[:,yy,xx] 
+        if cube_sig is not None:
+            matrix_sig = np.reshape(cube_sig, (n, -1))  
+   
 
-        for frame in range(n):
-            if ann_center > fwhm * 3:  # TODO: 3 optimal value? new par?
-                ind = _find_indices_adi(angle_list, frame, pa_thr,
-                                        truncate=True)
-            else:
-                ind = _find_indices_adi(angle_list, frame, pa_thr)
-                
+    # elif source_xy is None or delta_rot is None:
+    #     pass
+    # else:
+    #     # rotation threshold applied and NMF repeated for each frame on different matrices
+    #     if delta_rot is None or fwhm is None:
+    #         msg = 'Delta_rot or fwhm parameters missing. Needed for the'
+    #         msg += 'PA-based rejection of frames from the library'
+    #         raise TypeError(msg)
+    #     residuals_cube = np.zeros_like(cube)
+    #     recon_cube = np.zeros_like(cube)
+    #     yc, xc = frame_center(cube[0], False)
+    #     x1, y1 = source_xy
+    #     ann_center = dist(yc, xc, y1, x1)
+    #     pa_thr = _compute_pa_thresh(ann_center, fwhm, delta_rot)
+    #     mid_range = np.abs(np.amax(angle_list) - np.amin(angle_list))/2
+    #     if pa_thr >= mid_range - mid_range * 0.1:
+    #         new_pa_th = float(mid_range - mid_range * 0.1)
+    #         if verbose:
+    #             msg = 'PA threshold {:.2f} is too big, will be set to '
+    #             msg += '{:.2f}'
+    #             print(msg.format(pa_thr, new_pa_th))
+    #         pa_thr = new_pa_th
+
+    #     for frame in range(n):
+    #         if ann_center > fwhm * 3:  # TODO: 3 optimal value? new par?
+    #             ind = _find_indices_adi(angle_list, frame, pa_thr,
+    #                                     truncate=True)
+    #         else:
+    #             ind = _find_indices_adi(angle_list, frame, pa_thr)
+    
+
+    residuals_cube = np.zeros_like(array)
+    
     
     if source_xy is None:
-        residuals_result = _project_subtract(cube, None, ncomp, scaling,
-                                             mask_center_px, verbose, 
-                                             full_output, cube_sig=cube_sig, 
-                                             max_iter=max_iter, 
-                                             random_state=random_state, 
-                                             **kwargs)
+        residuals = _project_subtract(matrix, matrix_ref, ncomp, scaling,
+                                      mask_center_px, verbose, full_output, 
+                                      matrix_sig=matrix_sig, max_iter=max_iter, 
+                                      random_state=random_state, 
+                                      init_svd=init_svd, **kwargs)
         if verbose:
             timing(start_time)
         if full_output:
-            residuals_cube = residuals_result[0]
-            reconstructed = residuals_result[1]
-            H = residuals_result[2]
+            reconstructed = residuals[1]
+            H = residuals[2]
             recon_cube = reshape_matrix(reconstructed, y, x)
-        else:
-            residuals_cube = residuals_result
+            residuals = residuals[0]
+        for fr in range(n):
+            residuals_cube[fr][yy, xx] = residuals[fr]
     else:
         if delta_rot is None or fwhm is None:
             msg = 'Delta_rot or fwhm parameters missing. Needed for the'
             msg += 'PA-based rejection of frames from the library'
             raise TypeError(msg)
-        residuals_cube = np.zeros_like(cube)
         recon_cube = np.zeros_like(cube)
         yc, xc = frame_center(cube[0], False)
         x1, y1 = source_xy
@@ -149,34 +198,36 @@ def nmf(cube, angle_list, cube_ref=None, ncomp=1, scaling=None, max_iter=100,
                 print(msg.format(pa_thr, new_pa_th))
             pa_thr = new_pa_th
 
-        for frame in range(n):
+        for fr in range(n):
             if ann_center > fwhm * 3:  # TODO: 3 optimal value? new par?
-                ind = _find_indices_adi(angle_list, frame, pa_thr,
+                ind = _find_indices_adi(angle_list, fr, pa_thr,
                                         truncate=True)
             else:
-                ind = _find_indices_adi(angle_list, frame, pa_thr)
+                ind = _find_indices_adi(angle_list, fr, pa_thr)
 
-            res_result = _project_subtract(cube, None, ncomp, scaling,
+            res_result = _project_subtract(matrix, matrix_ref, ncomp, scaling,
                                            mask_center_px, verbose, 
-                                           full_output, ind, frame, 
-                                           cube_sig=cube_sig, max_iter=max_iter, 
-                                           random_state=random_state, **kwargs)
+                                           full_output, ind, fr, 
+                                           matrix_sig=matrix_sig, 
+                                           max_iter=max_iter, 
+                                           random_state=random_state, 
+                                           init_svd=init_svd, **kwargs)
+            # ! Instead of reshaping, fill frame using get_annulus?
             if full_output:
-                residual_frame = res_result[0]
+                residuals = res_result[0]
                 recon_frame = res_result[1]
-                H = residuals_result[2]
-                residuals_cube[frame] = residual_frame.reshape((y, x))
-                recon_cube[frame] = recon_frame.reshape((y, x))
+                H = res_result[2]
+                recon_cube[fr] = recon_frame.reshape((y, x))
             else:
-                residual_frame = res_result
-                residuals_cube[frame] = residual_frame.reshape((y, x))
-     
+                residuals = res_result
+            residuals_cube[fr][yy, xx] = residuals[fr]
+                
     if verbose:  
         print('Done NMF with sklearn.NMF.')
         timing(start_time)
             
     residuals_cube_ = cube_derotate(residuals_cube, angle_list, imlib=imlib,
-                              interpolation=interpolation)
+                                    interpolation=interpolation)
     frame = cube_collapse(residuals_cube_, mode=collapse)
     
     if verbose:  
@@ -189,9 +240,10 @@ def nmf(cube, angle_list, cube_ref=None, ncomp=1, scaling=None, max_iter=100,
         return frame
     
     
-def _project_subtract(cube, cube_ref, ncomp, scaling, mask_center_px, verbose, 
-                      full_output, indices=None, frame=None, cube_sig=None,
-                      max_iter=100, random_state=None, **kwargs):
+def _project_subtract(matrix, matrix_ref, ncomp, scaling, mask_center_px, 
+                      verbose, full_output, indices=None, frame=None, 
+                      matrix_sig=None, max_iter=100, random_state=None, 
+                      init_svd='nndsvd', **kwargs):
     """
     PCA projection and model PSF subtraction. Used as a helping function by
     each of the PCA modes (ADI, ADI+RDI, ADI+mSDI).
@@ -234,36 +286,33 @@ def _project_subtract(cube, cube_ref, ncomp, scaling, mask_center_px, verbose,
     V : numpy ndarray
         [full_output=True, indices is None, frame is None] The right singular
         vectors of the input matrix, as returned by ``svd/svd_wrapper()``
-    """
-    n, y, x = cube.shape
-    # if cube_sig is not None:
-    #     cube_emp = cube-cube_sig
-    # else:
-    #     cube_emp = None
-    if indices is not None and frame is not None:
-        matrix = prepare_matrix(cube, scaling, mask_center_px,
-                                mode='fullfr', verbose=False)
-    else:
-        matrix = prepare_matrix(cube, scaling, mask_center_px,
-                                mode='fullfr', verbose=verbose)
-    if cube_sig is None:
+    """    
+    if matrix_sig is None:
         matrix_emp = matrix.copy()
     else:
-        nfr = cube_sig.shape[0]
-        cube_sig = np.reshape(cube_sig, (nfr, -1))
-        matrix_emp = matrix-cube_sig
-
-    if cube_ref is not None:
-        ref_lib = prepare_matrix(cube_ref, scaling, mask_center_px,
-                                 mode='fullfr', verbose=verbose)
+        matrix_emp = matrix-matrix_sig
+        
+    if matrix_ref is not None:
+        ref_lib = matrix_ref
+    elif indices is not None and frame is not None:
+        ref_lib = matrix_emp[indices]
     else:
         ref_lib = matrix_emp
 
-    mod = NMF(n_components=ncomp, alpha=0, solver='cd', init='nndsvd', 
+    # to avoid bug, just consider positive values
+    if np.median(ref_lib)<0:
+        raise ValueError("Mostly negative values in the cube")
+    else:
+        ref_lib[np.where(ref_lib<0)] = 0
+
+    if init_svd == 'nndsvda':
+        solver = 'mu'
+    else:
+        solver = 'cd'
+    mod = NMF(n_components=ncomp, alpha=0, solver=solver, init=init_svd, 
               max_iter=max_iter, random_state=random_state, **kwargs)   
     # a rotation threshold is used (frames are processed one by one)
     if indices is not None and frame is not None:
-        ref_lib = ref_lib[indices]
         if ref_lib.shape[0] <= 10:
             raise RuntimeError('Less than 10 frames left in the PCA library'
                                ', Try decreasing the parameter delta_rot')
@@ -279,7 +328,7 @@ def _project_subtract(cube, cube_ref, ncomp, scaling, mask_center_px, verbose,
         reconstructed = np.dot(W, H)
         residuals = curr_frame - reconstructed
         if full_output:
-            return H, residuals, reconstructed
+            return residuals, reconstructed, H
         else:
             return residuals
 
@@ -292,14 +341,10 @@ def _project_subtract(cube, cube_ref, ncomp, scaling, mask_center_px, verbose,
         #    H = mod.fit(matrix).components_          
         
         # W: coefficients [n_frames, ncomp]
-        W = mod.transform(cube)
+        W = mod.transform(matrix_emp)
             
         reconstructed = np.dot(W, H)
         residuals = matrix - reconstructed
-        
-        array_out = np.zeros_like(cube)
-        for i in range(n):
-            array_out[i] = residuals[i].reshape(y,x)
         
         # V = svd_wrapper(ref_lib, svd_mode, ncomp, verbose)
         # transformed = np.dot(V, matrix_emp.T)
@@ -307,8 +352,6 @@ def _project_subtract(cube, cube_ref, ncomp, scaling, mask_center_px, verbose,
         # residuals = matrix - reconstructed
         # residuals_res = reshape_matrix(residuals, y, x)
         if full_output:
-            return array_out, reconstructed, H
+            return residuals, reconstructed, H
         else:
-            return array_out
-
-
+            return residuals

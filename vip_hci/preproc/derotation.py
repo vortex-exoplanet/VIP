47a8e962773a15de9bf6c8af34b7287311a25cca
IainHammond <Iain.Hammond@monash.edu>
2023-01-16 15:30:41 +1100

    implemented dict to frame_rotate

--- vip_hci/preproc/derotation.py
+++ vip_hci/preproc/derotation.py
@@ -41,7 +41,7 @@ except ImportError:
 
 def frame_rotate(array, angle, imlib='vip-fft', interpolation='lanczos4',
                  cxy=None, border_mode='constant', mask_val=np.nan,
-                 edge_blend=None, interp_zeros=False, ker=1):
+                 edge_blend=None, interp_zeros=False, ker=1, **rot_options):
     """ Rotates a frame or 2D array.
 
     Parameters
@@ -50,7 +50,7 @@ def frame_rotate(array, angle, imlib='vip-fft', interpolation='lanczos4',
         Input image, 2d array.
     angle : float
         Rotation angle.
-    imlib : {'opencv', 'skimage', 'vip-fft'}, str optional
+    imlib : {'opencv', 'skimage', 'vip-fft'}, str, optional
         Library used for image transformations. Opencv is faster than
         Skimage or scipy.ndimage. 'vip-fft' corresponds to the FFT-based
         rotation method described in [LAR97]_, and implemented in this
@@ -69,9 +69,9 @@ def frame_rotate(array, angle, imlib='vip-fft', interpolation='lanczos4',
         Opencv and 'biquartic' for Skimage.
     cxy : float, optional
         Coordinates X,Y  of the point with respect to which the rotation will be
-        performed. By default the rotation is done with respect to the center
+        performed. By default, the rotation is done with respect to the center
         of the frame.
-    border_mode : {'constant', 'edge', 'symmetric', 'reflect', 'wrap'}, str opt
+    border_mode : {'constant', 'edge', 'symmetric', 'reflect', 'wrap'}, str, optional
         Pixel extrapolation method for handling the borders. 'constant' for
         padding with zeros. 'edge' for padding with the edge values of the
         image. 'symmetric' for padding with the reflection of the vector
@@ -80,11 +80,11 @@ def frame_rotate(array, angle, imlib='vip-fft', interpolation='lanczos4',
         vector along each axis. 'wrap' for padding with the wrap of the vector
         along the axis (the first values are used to pad the end and the end
         values are used to pad the beginning). Default is 'constant'.
-    mask_val: flt, opt
+    mask_val: float, optional
         If any numerical mask in the image to be rotated, what are its values?
         Will only be used if a strategy to mitigate Gibbs effects is adopted -
         see below.
-    edge_blend: str, opt {None,'noise','interp','noise+interp'}
+    edge_blend: str, optional {None,'noise','interp','noise+interp'}
         Whether to blend the edges, by padding nans then inter/extrapolate them
         with a gaussian filter. Slower but can significantly reduce ringing
         artefacts from Gibbs phenomenon, in particular if several consecutive
@@ -95,13 +95,17 @@ def frame_rotate(array, angle, imlib='vip-fft', interpolation='lanczos4',
         - 'noise+interp': sum both components above at masked locations.
         
         Original mask will be placed back after rotation.
-    interp_zeros: bool, opt
+    interp_zeros: bool, optional
         [only used if edge_blend is not None]
         Whether to interpolate zeros in the frame before (de)rotation. Not
         dealing with them can induce a Gibbs phenomenon near their location.
         However, this flag should be false if rotating a binary mask.
-    ker: float, opt
+    ker: float, optional
         Size of the Gaussian kernel used for interpolation.
+    rot_options: dict, optional
+        Dictionary of all the above options, for the purpose of propagating
+        rotation options through VIP functions. Note any arguments here will
+        override the default parameter.
 
     Returns
     -------
@@ -111,6 +115,15 @@ def frame_rotate(array, angle, imlib='vip-fft', interpolation='lanczos4',
     """
     if array.ndim != 2:
         raise TypeError('Input array is not a frame or 2d array')
+
+    imlib = rot_options.get('imlib', imlib)
+    interpolation = rot_options.get('interpolation', interpolation)
+    cxy = rot_options.get('cxy', cxy)
+    border_mode = rot_options.get('border_mode', border_mode)
+    mask_val = rot_options.get('mask_val', mask_val)
+    edge_blend = rot_options.get('edge_blend', edge_blend)
+    interp_zeros = rot_options.get('interp_zeros', interp_zeros)
+    ker = rot_options.get('ker', ker)
 
     if edge_blend is None:
         edge_blend = ''
@@ -315,11 +328,11 @@ def frame_rotate(array, angle, imlib='vip-fft', interpolation='lanczos4',
 
 def cube_derotate(array, angle_list, imlib='vip-fft', interpolation='lanczos4',
                   cxy=None, nproc=1, border_mode='constant', mask_val=np.nan,
-                  edge_blend=None, interp_zeros=False, ker=1):
-    """ Rotates an cube (3d array or image sequence) providing a vector or
+                  edge_blend=None, interp_zeros=False, ker=1, **rot_options):
+    """ Rotates a cube (3d array or image sequence) providing a vector or
     corresponding angles. Serves for rotating an ADI sequence to a common north
     given a vector with the corresponding parallactic angles for each frame. By
-    default bicubic interpolation is used (opencv).
+    default, bicubic interpolation is used (opencv).
 
     Parameters
     ----------
@@ -333,7 +346,7 @@ def cube_derotate(array, angle_list, imlib='vip-fft', interpolation='lanczos4',
         See the documentation of the ``vip_hci.preproc.frame_rotate`` function.
     cxy : tuple of int, optional
         Coordinates X,Y  of the point with respect to which the rotation will be
-        performed. By default the rotation is done with respect to the center
+        performed. By default, the rotation is done with respect to the center
         of the frames, as it is returned by the function
         vip_hci.var.frame_center.
     nproc : int, optional
@@ -350,7 +363,8 @@ def cube_derotate(array, angle_list, imlib='vip-fft', interpolation='lanczos4',
         See the documentation of the ``vip_hci.preproc.frame_rotate`` function.
     ker: int, optional
         See the documentation of the ``vip_hci.preproc.frame_rotate`` function.
-
+    rot_options: dict, optional
+        See the documentation of the ``vip_hci.preproc.frame_rotate`` function.
     Returns
     -------
     array_der : numpy ndarray
@@ -372,22 +386,24 @@ def cube_derotate(array, angle_list, imlib='vip-fft', interpolation='lanczos4',
                                         border_mode=border_mode,
                                         mask_val=mask_val,
                                         edge_blend=edge_blend,
-                                        interp_zeros=interp_zeros, ker=ker)
+                                        interp_zeros=interp_zeros, ker=ker,
+                                        **rot_options)
     elif nproc > 1:
 
         res = pool_map(nproc, _frame_rotate_mp, iterable(array),
                        iterable(-angle_list), imlib, interpolation, cxy,
-                       border_mode, mask_val, edge_blend, interp_zeros, ker)
+                       border_mode, mask_val, edge_blend, interp_zeros, ker,
+                       **rot_options)
         array_der = np.array(res)
 
     return array_der
 
 
 def _frame_rotate_mp(frame, angle, imlib, interpolation, cxy, border_mode,
-                     mask_val, edge_blend, interp_zeros, ker):
+                     mask_val, edge_blend, interp_zeros, ker, **rot_options):
     framerot = frame_rotate(frame, angle, imlib, interpolation, cxy,
                             border_mode, mask_val, edge_blend, interp_zeros,
-                            ker)
+                            ker, **rot_options)
     return framerot
 
 

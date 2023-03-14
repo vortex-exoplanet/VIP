85c6e3911c229dfde77ab5f52d447d4c0591dd72
Thomas Bédrine <tbedrine@gmail.com>
2023-03-14 09:53:16 +0100

    Created an object for frame diff algo (+ minor doc changes)

--- vip_hci/postproc.py
+++ vip_hci/postproc.py
@@ -2,7 +2,15 @@
 """Module with the HCI<post-processing algorithms> classes."""
 
 __author__ = "Carlos Alberto Gomez Gonzalez, Ralf Farkas"
-__all__ = ["PPMedianSub", "PPPcaFF", "PPLoci", "PPLLSG", "PPAndromeda"]
+__all__ = [
+    "PPFrameDiff",
+    "PPMedianSub",
+    "PPPcaFF",
+    "PPPcaAnn",
+    "PPLoci",
+    "PPLLSG",
+    "PPAndromeda",
+]
 
 import pickle
 import numpy as np
@@ -11,7 +19,7 @@ from sklearn.base import BaseEstimator
 from .dataset import Dataset
 from .metrics import snrmap
 from .invprob import andromeda
-from .psfsub import pca, llsg, median_sub, xloci, pca_annular
+from .psfsub import pca, llsg, median_sub, xloci, pca_annular, frame_diff
 from .config.utils_conf import algo_calculates_decorator as calculates
 
 # TODO : cross-check every algorithm validity
@@ -301,6 +309,123 @@ class PostProc(BaseEstimator):
         raise NotImplementedError
 
 
+class PPFrameDiff(PostProc):
+    """Post-processing frame differencing algorithm."""
+
+    def __init__(
+        self,
+        dataset=None,
+        metric="manhattan",
+        dist_threshold=50,
+        n_similar=None,
+        delta_rot=0.5,
+        radius_int=2,
+        asize=4,
+        ncomp=None,
+        imlib="vip-fft",
+        interpolation="lanczos4",
+        collapse="median",
+        nproc=1,
+        verbose=True,
+    ):
+        """
+        Set up the frame differencing algorithm parameters.
+
+        Parameters
+        ----------
+        dataset : Dataset object
+            A Dataset object to be processed.
+        metric : str, optional
+            Distance metric to be used ('cityblock', 'cosine', 'euclidean', 'l1',
+            'l2', 'manhattan', 'correlation', etc). It uses the scikit-learn
+            function ``sklearn.metrics.pairwise.pairwise_distances`` (check its
+            documentation).
+        dist_threshold : int
+            Indices with a distance larger than ``dist_threshold`` percentile will
+            initially discarded.
+        n_similar : None or int, optional
+            If a postive integer value is given, then a median combination of
+            ``n_similar`` frames will be used instead of the most similar one.
+        delta_rot : int
+            Minimum parallactic angle distance between the pairs.
+        radius_int : int, optional
+            The radius of the innermost annulus. By default is 0, if >0 then the
+            central circular area is discarded.
+        asize : int, optional
+            The size of the annuli, in pixels.
+        ncomp : None or int, optional
+            If a positive integer value is given, then the annulus-wise PCA low-rank
+            approximation with ``ncomp`` principal components will be subtracted.
+            The pairwise subtraction will be performed on these residuals.
+        nproc : None or int, optional
+            Number of processes for parallel computing. If None the number of
+            processes will be set to cpu_count()/2. By default the algorithm works
+            in single-process mode.
+        imlib : str, opt
+            See description in vip.preproc.frame_rotate()
+        interpolation : str, opt
+            See description in vip.preproc.frame_rotate()
+        collapse: str, opt
+            What to do with derotated residual cube? See options of
+            vip.preproc.cube_collapse()
+        verbose : bool, optional
+            If True prints to stdout intermediate info.
+
+        """
+        super(PPFrameDiff, self).__init__(locals())
+
+    @calculates("frame_final")
+    def run(self, dataset=None, nproc=1, full_output=True, verbose=True, **rot_options):
+        """
+        Run the post-processing median subtraction algorithm for model PSF subtraction.
+
+        Parameters
+        ----------
+        dataset : Dataset object
+            A Dataset object to be processed.
+        nproc : None or int, optional
+            Number of processes for parallel computing. If None the number of
+            processes will be set to cpu_count()/2. By default the algorithm works
+            in single-process mode.
+        full_output: bool, optional
+            Whether to return the final median combined image only or with other
+            intermediate arrays.
+        verbose : bool, optional
+            If True prints to stdout intermediate info.
+        rot_options: dictionary, optional
+            Dictionary with optional keyword values for "border_mode", "mask_val",
+            "edge_blend", "interp_zeros", "ker" (see documentation of
+            ``vip_hci.preproc.frame_rotate``).
+
+        """
+        dataset = self._get_dataset(dataset, verbose)
+
+        if dataset.fwhm is None:
+            raise ValueError("`fwhm` has not been set")
+
+        res = frame_diff(
+            cube=dataset.cube,
+            angle_list=dataset.angles,
+            fwhm=dataset.fwhm,
+            metric=self.metric,
+            dist_threshold=self.dist_threshold,
+            n_similar=self.n_similar,
+            delta_rot=self.delta_rot,
+            radius_int=self.radius_int,
+            asize=self.asize,
+            ncomp=self.ncomp,
+            imlib=self.imlib,
+            interpolation=self.interpolation,
+            collapse=self.collapse,
+            nproc=nproc,
+            full_output=True,
+            verbose=verbose,
+            **rot_options
+        )
+
+        self.frame_final = res
+
+
 class PPMedianSub(PostProc):
     """Post-processing median subtraction algorithm."""
 
@@ -325,6 +450,8 @@ class PPMedianSub(PostProc):
 
         Parameters
         ----------
+        dataset : Dataset object
+            A Dataset object to be processed.
         flux_sc_list : numpy ndarray, 1d
             In the case of IFS data (ADI+SDI), this is the list of flux scaling
             factors applied to each spectral frame after geometrical rescaling.

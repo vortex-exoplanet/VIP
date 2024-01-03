b78f1c6419992c1275ebc86eca1def0c636b10bc
VChristiaens <valentinchrist@hotmail.com>
2024-01-03 15:42:53 +0100

    Added IPCA and IROLL routines

---
+++ vip_hci/greedy/__init__.py
@@ -0,0 +1,10 @@
+"""
+Subpackage ``greedy`` contains iterative implementations of stellar PSF
+modelling + subtraction algorithms. The following methods have been implemented:
+- iterative roll subtraction [HEA00]_
+- iterative PCA in full frame [PAI18]_ / [PAI21]_
+- iterative NMF in full frame (cite latest VIP paper if used).
+"""
+from .inmf_fullfr import *
+from .ipca_fullfr import *
+from .iroll import *

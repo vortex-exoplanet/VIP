4f697ef59671b78eea934b10d9b6237f9fba495b
VChristiaens <valentinchrist@hotmail.com>
2024-01-18 11:53:51 +0100

    Updated npc used in negfc test

--- tests/pre_3_10/test_fm_negfc_3d.py
+++ tests/pre_3_10/test_fm_negfc_3d.py
@@ -67,7 +67,7 @@ def injected_cube_position(example_dataset_adi):
     "pca_algo, negfc_algo, ncomp, mu_sigma, fm, force_rpa, conv_test",
     [
         (pca_annular, firstguess, 3, False, "stddev", False, None),
-        (pca, firstguess, 3, True, None, False, None),
+        (pca, firstguess, 2, True, None, False, None),
         (median_sub, firstguess, None, False, "sum", False, None),
         (pca_annulus, mcmc_negfc_sampling, 3, False, "stddev", False, "gb"),
         (pca_annulus, mcmc_negfc_sampling, 3, True, None, True, "ac"),

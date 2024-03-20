57803545360af09b8f66451dd427bacfdb7d5084
VChristiaens <valentinchrist@hotmail.com>
2024-03-20 21:25:18 +0100

    Added error message in case input array is not 2 or 3D

--- vip_hci/fm/fakecomp.py
+++ vip_hci/fm/fakecomp.py
@@ -1,7 +1,5 @@
 #! /usr/bin/env python
-"""
-Module with fake companion injection functions.
-"""
+"""Module with fake companion injection functions."""
 
 __author__ = 'Carlos Alberto Gomez Gonzalez, Valentin Christiaens'
 __all__ = ['collapse_psf_cube',
@@ -38,7 +36,8 @@ def cube_inject_companions(array, psf_template, angle_list, flevel, rad_dists,
                            interpolation='lanczos4', transmission=None,
                            radial_gradient=False, full_output=False,
                            verbose=False, nproc=1):
-    """ Injects fake companions in branches, at given radial distances.
+    """Inject fake companions in branches and given radial distances in an ADI\
+    cube.
 
     Parameters
     ----------
@@ -165,14 +164,6 @@ def cube_inject_companions(array, psf_template, angle_list, flevel, rad_dists,
                                                  -(ang*180/np.pi-angle_list[0]),
                                                  imlib=imlib_rot,
                                                  interpolation=interpolation)
-
-                        # shift_y = rad * np.sin(ang - np.deg2rad(angle_list[0]))
-                        # shift_x = rad * np.cos(ang - np.deg2rad(angle_list[0]))
-                        # dsy = shift_y-int(shift_y)
-                        # dsx = shift_x-int(shift_x)
-                        # fc_fr_ang = frame_shift(psf_trans, dsy, dsx, imlib_sh,
-                        #                         interpolation,
-                        #                         border_mode='constant')
                     else:
                         fc_fr_rad = interp_trans(rad)*fc_fr
                 if nproc == 1:
@@ -247,7 +238,7 @@ def cube_inject_companions(array, psf_template, angle_list, flevel, rad_dists,
         if transmission.ndim != 2:
             raise ValueError("transmission should be a 2D ndarray")
         elif t_nz != 2 and t_nz != 1+array.shape[0]:
-            msg = "transmission dimensions should be either (2,N) or (n_wave+1, N)"
+            msg = "transmission dimensions should be (2,N) or (n_wave+1, N)"
             raise ValueError(msg)
         # if transmission doesn't have right format for interpolation, adapt it
         diag = np.sqrt(2)*array.shape[-1]
@@ -324,10 +315,7 @@ def cube_inject_companions(array, psf_template, angle_list, flevel, rad_dists,
 def _frame_shift_fcp(fc_fr_rad, array, rad, ang, derot_ang, flevel, size_fc,
                      imlib_sh, imlib_rot, interpolation, transmission,
                      radial_gradient):
-    """
-    Specific cube shift algorithm for injection of fake companions
-    """
-
+    """Specific cube shift algorithm to inject fake companions."""
     ceny, cenx = frame_center(array)
     sizey = array.shape[-2]
     sizex = array.shape[-1]
@@ -480,9 +468,9 @@ def generate_cube_copies_with_injections(array, psf_template, angle_list, plsc,
 def frame_inject_companion(array, array_fc, pos_y, pos_x, flux,
                            imlib='vip-fft', interpolation='lanczos4'):
     """
-    Injects a fake companion in a single frame (it could be a single
-    multi-wavelength frame) at given coordinates, or in a cube (at the same
-    coordinates, flux and with same fake companion image throughout the cube).
+    Inject a fake companion in a single frame (can be a single multi-wavelength\
+    frame) at given coordinates, or in a cube (at the same coordinates, flux\
+    and with same fake companion image throughout the cube).
 
     Parameters
     ----------
@@ -539,7 +527,7 @@ def frame_inject_companion(array, array_fc, pos_y, pos_x, flux,
 
 
 def collapse_psf_cube(array, size, fwhm=4, verbose=True, collapse='mean'):
-    """ Creates a 2d PSF template from a cube of non-saturated off-axis frames
+    """Create a 2d PSF template from a cube of non-saturated off-axis frames\
     of the star by taking the mean and normalizing the PSF flux.
 
     Parameters
@@ -560,6 +548,7 @@ def collapse_psf_cube(array, size, fwhm=4, verbose=True, collapse='mean'):
     -------
     psf_normd : numpy ndarray
         Normalized PSF.
+
     """
     if array.ndim != 3 and array.ndim != 4:
         raise TypeError('Array is not a cube, 3d or 4d array.')
@@ -584,9 +573,11 @@ def normalize_psf(array, fwhm='fit', size=None, threshold=None, mask_core=None,
                   model='gauss', imlib='vip-fft', interpolation='lanczos4',
                   force_odd=True, correct_outliers=True, full_output=False,
                   verbose=True, debug=False):
-    """ Normalizes a PSF (2d or 3d array), to have the flux in a 1xFWHM
-    aperture equal to one. It also allows to crop the array and center the PSF
-    at the center of the array(s).
+    """Normalize a PSF (2d or 3d array), to have the flux in a 1xFWHM aperture\
+    equal to one.
+
+    It also allows cropping of the array. Automatic recentering of the PSF is
+    done internally - as long as it is already roughly centered within ~2px.
 
     Parameters
     ----------
@@ -599,8 +590,8 @@ def normalize_psf(array, fwhm='fit', size=None, threshold=None, mask_core=None,
         estimate the FWHM in 2D or 3D PSF arrays.
     size : int or None, optional
         If int it will correspond to the size of the centered sub-image to be
-        cropped form the PSF array. The PSF is assumed to be roughly centered wrt
-        the array.
+        cropped form the PSF array. The PSF is assumed to be roughly centered
+        with respect to the array.
     threshold : None or float, optional
         Sets to zero values smaller than threshold (in the normalized image).
         This can be used to only leave the core of the PSF.
@@ -646,7 +637,7 @@ def normalize_psf(array, fwhm='fit', size=None, threshold=None, mask_core=None,
 
     """
     def psf_norm_2d(psf, fwhm, threshold, mask_core, full_output, verbose):
-        """ 2d case """
+        """Normalize PSF in the 2d case."""
         # we check if the psf is centered and fix it if needed
         cy, cx = frame_center(psf, verbose=False)
         xcom, ycom = cen_com(psf)
@@ -766,8 +757,9 @@ def normalize_psf(array, fwhm='fit', size=None, threshold=None, mask_core=None,
             if fwhm != 'fit':
                 fwhm = [fwhm] * array.shape[0]
             else:
-                fits_vect = [fit_2d(array[i], full_output=True, debug=debug) for i
-                             in range(n)]
+                fits_vect = [fit_2d(array[i],
+                                    full_output=True,
+                                    debug=debug) for i in range(n)]
                 if model == 'gauss':
                     fwhmx = [fits_vect[i]['fwhm_x'] for i in range(n)]
                     fwhmy = [fits_vect[i]['fwhm_y'] for i in range(n)]
@@ -792,8 +784,8 @@ def normalize_psf(array, fwhm='fit', size=None, threshold=None, mask_core=None,
                                 fwhm[f] = np.nanmean(np.array([fwhm[f-1],
                                                                fwhm[f+1]]))
                             elif np.isnan(fwhm[f]):
-                                msg = "2D fit failed for first or last channel. "
-                                msg += "Try other parameters?"
+                                msg = "2D fit failed for first or last channel."
+                                msg += " Try other parameters?"
                                 raise ValueError(msg)
         elif len(fwhm) != array.shape[0]:
             msg = "If fwhm is a list/1darray it should have a length of {}"
@@ -817,3 +809,9 @@ def normalize_psf(array, fwhm='fit', size=None, threshold=None, mask_core=None,
             return array_out, fwhm_flux, fwhm
         else:
             return array_out
+
+    else:
+        msg = "Input psf should be 2D or 3D. If of higher dimension, either use"
+        msg += " ``vip_hci.preproc.cube_collapse`` first, or loop on the "
+        msg += "temporal axis."
+        raise ValueError(msg.format(array.shape[0]))

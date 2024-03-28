4f06c734fcba39f2de60e587c85f560e65bea37c
VChristiaens <valentinchrist@hotmail.com>
2024-03-28 21:34:57 +0100

    Updated ncomp used in NEGFC test

--- tests/pre_3_10/test_fm_negfc_3d.py
+++ tests/pre_3_10/test_fm_negfc_3d.py
@@ -70,8 +70,8 @@ def injected_cube_position(example_dataset_adi):
         (pca, firstguess, 5, True, None, False, None),
         (median_sub, firstguess, None, False, "sum", False, None),
         (pca_annulus, mcmc_negfc_sampling, 2, False, "stddev", False, "gb"),
-        (pca_annulus, mcmc_negfc_sampling, 3, True, None, True, "ac"),
-        (pca_annulus, nested_negfc_sampling, 3, False, "sum", False, None),
+        (pca_annulus, mcmc_negfc_sampling, 2, True, None, True, "ac"),
+        (pca_annulus, nested_negfc_sampling, 2, False, "sum", False, None),
     ],
 )
 def test_algos(

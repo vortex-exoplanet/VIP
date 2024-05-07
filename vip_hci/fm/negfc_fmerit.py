86d6b63a489b76ab074882a15c0489ffd3815c32
IainHammond <Iain.Hammond@monash.edu>
2024-05-07 11:34:31 +1000

    get_mu_and_sigma: radius_int should behave as expected when not provided

--- vip_hci/fm/negfc_fmerit.py
+++ vip_hci/fm/negfc_fmerit.py
@@ -714,6 +714,10 @@ def get_mu_and_sigma(
     if r_guess > centx_fr - halfw:  # or r_guess <= halfw:
         raise RuntimeError(msg)
 
+    # check if r_guess is less than fwhm
+    if r_guess < fwhm:
+        raise RuntimeError("r_guess should be greater than fwhm.")
+
     ncomp = algo_options.get("ncomp", ncomp)
     svd_mode = algo_options.get("svd_mode", svd_mode)
     scaling = algo_options.get("scaling", scaling)
@@ -721,8 +725,7 @@ def get_mu_and_sigma(
     interpolation = algo_options.get("interpolation", interpolation)
     collapse = algo_options.get("collapse", collapse)
 
-    radius_int = max(1, int(np.floor(r_guess - annulus_width / 2)))
-    radius_int = algo_options.get("radius_int", radius_int)
+    radius_int = algo_options.get("radius_int", 0)
 
     # not recommended, except if large-scale residual sky present (NIRC2-L')
     hp_filter = algo_options.get("hp_filter", None)
@@ -890,9 +893,9 @@ def get_mu_and_sigma(
     else:
         raise TypeError("Wedge should have exactly 2 values")
 
-    indices = get_annular_wedge(pca_res, radius_int, 2 * fwhm, wedge=wedge)
+    indices = get_annular_wedge(pca_res, r_guess - fwhm, 2 * fwhm, wedge=wedge)
     yy, xx = indices
-    indices_inv = get_annular_wedge(pca_res_inv, radius_int, 2 * fwhm,
+    indices_inv = get_annular_wedge(pca_res_inv, r_guess - fwhm, 2 * fwhm,
                                     wedge=wedge)
     yyi, xxi = indices_inv
     all_res = np.concatenate((pca_res[yy, xx], pca_res_inv[yyi, xxi]))
